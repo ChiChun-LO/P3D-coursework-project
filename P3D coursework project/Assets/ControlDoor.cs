fbdba322bdbf0ed042ce527b172823715669ad95
ChiChun-LO <cl722@sussex.ac.uk>
2023-01-09 17:09:32 +0000

    Door

--- P3D coursework project/Assets/ControlDoor.cs
+++ P3D coursework project/Assets/ControlDoor.cs
@@ -15,55 +15,20 @@ public class ControlDoor : MonoBehaviour
         
         if(other.tag == "Player")
         {
-
-        if(/*Input.GetMouseButtonUp(0) &&*/ open)
-        {
-            myLeftDoor.Play("OpenDoor", 0, 0.0f);
-            myRightDoor.Play("OpenDoor 1", 0, 0.0f);
-            gameObject.SetActive(false);
-            
-        }
-
-        else if(/*Input.GetMouseButtonUp(0) &&*/ close)
-        {
-            myLeftDoor.Play("CloseDoor", 0, 0.0f);
-            myRightDoor.Play("CloseDoor 1", 0, 0.0f);
-            gameObject.SetActive(false);
-        }
-        }
-    }
-
-
-    /*void Update()d
-    {
-        if(Input.GetMouseButtonUp(0))
-        {
-            Debug.Log("E");
-        }
-    }*/
-}
-    /*private void OnTriggerExit(Collider other)
-    {
-        //Debug.Log("Exiting here");
-        if (other.tag == playerTag)
-        {
-            inTrigger = false;
-            animator.SetTrigger(hibernateTrigger);
-        }
-    }*/
-/*
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.E))
+            if(open)
             {
-                Debug.Log("E");
+                myLeftDoor.Play("OpenDoor", 0, 0.0f);
+                myRightDoor.Play("OpenDoor 1", 0, 0.0f);
+                gameObject.SetActive(false);
+            
             }
+
+            else if(close)
+            {
+                myLeftDoor.Play("CloseDoor", 0, 0.0f);
+                myRightDoor.Play("CloseDoor 1", 0, 0.0f);
+                gameObject.SetActive(false);
+            }
+        }
     }
-}}*/
+}
